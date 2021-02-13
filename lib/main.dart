23c707fef3bdc0f18f3e8d6d652bd738b21bf8a5
Acderin1914 <64862787+Acderin1914@users.noreply.github.com>
2021-02-13 21:39:42 +0300

    true oldu

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.red,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: FirebaseInitialize(),
     );
   }
